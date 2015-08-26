2f44165d0076e23e869e94910e33670de0f077ff
Ming-der Wang <ming@log4analytics.com>
2015-08-26 19:07:59 +0800

    save file to /tmp/ming.go

--- Makefile
+++ Makefile
@@ -1,19 +1,13 @@
 .PHONY: all clean
 
 all: onion.go 
-	@go generate
 	@go build
-	sudo ./onion serve
+	./onion serve
 
 cleandb: Onion.db
 	rm -f Onion.db
 clean:
 	@go clean
-	rm -f onion_resource.go
-	rm -f web_service.go
-	rm -f main.go
-	tree
-	cat onion.go
 
 migrate:
 	./onion migratedb
--- onion.go
+++ onion.go
@@ -1,11 +1,9 @@
-//go:generate ginger $GOFILE
 package main
 
-//@ginger
 type Onion struct {
-	Ginger_Created int32  `json:"ginger_created"`
-	Ginger_Id      int32  `json:"ginger_id" gorm:"primary_key"`
+	Ginger_Created int32 `json:"ginger_created"`
+	Ginger_Id      int32 `json:"ginger_id" gorm:"primary_key"`
 
-	Address        string `json:"address"`
-	Exit           string `json:"exit"`
+	TypeName   string `json:"typeName"`
+	JsonSchema string `json:"jsonSchema"`
 }
