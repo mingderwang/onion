4433ea6b883bb13f45d307720efd597299b08d7a
Ming-der Wang <ming@log4analytics.com>
2015-09-02 23:50:52 +0800

    ok for test with gcloud docker push

--- docker.sh
+++ docker.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-cd $1; go generate
+cd $1
+go generate
+#don't build docker locally as following script.
+#./dockerize.sh
+
+docker build -t gcr.io/mitac-cust-gcp-1/demo-onion:v4 .
+gcloud docker push gcr.io/mitac-cust-gcp-1/demo-onion:v4
