6688a34984f2eaf240a95ed2c990baeb0a6c2080
Ming-der Wang <mingderwang@gmail.com>
2015-09-03 08:16:11 +0000

    final RC 1 for demo

--- docker.sh
+++ docker.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 cd $1
+
 go generate
 #don't build docker locally as following script.
 #./dockerize.sh
 
-docker build -t gcr.io/mitac-cust-gcp-1/demo-onion:v4 .
-gcloud docker push gcr.io/mitac-cust-gcp-1/demo-onion:v4
+typeset -i variable=$(cat version)
+((variable=variable+1))
+echo $variable > version
+
+docker build -t gcr.io/mitac-cust-gcp-1/demo-onion:v$variable .
+gcloud docker push gcr.io/mitac-cust-gcp-1/demo-onion:v$variable
+kubectl rolling-update demo-node --image=gcr.io/mitac-cust-gcp-1/demo-onion:v$variable
+
