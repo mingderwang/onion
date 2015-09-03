a3bce49e46c011caca548c850edc4b568935bfea
Ming-der Wang <mingderwang@gmail.com>
2015-09-03 09:18:12 +0000

    final RC2 ok

--- docker.sh
+++ docker.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
+
+typeset -i variable=$(cat version)
+((variable=variable+1))
+echo $variable > version
+
 cd $1
+
+FILE=Makefile
+
+if [ -f $FILE ];
+then
+   echo "File $FILE exists."
+   make clean
+fi
+
 
 go generate
 #don't build docker locally as following script.
 #./dockerize.sh
 
-typeset -i variable=$(cat version)
-((variable=variable+1))
-echo $variable > version
 
 docker build -t gcr.io/mitac-cust-gcp-1/demo-onion:v$variable .
 gcloud docker push gcr.io/mitac-cust-gcp-1/demo-onion:v$variable
