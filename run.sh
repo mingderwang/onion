823d74513f36d385a04127efb87f1a37c9363062
Ming-der Wang <ming@log4analytics.com>
2015-09-16 09:11:20 +0800

    add gcloud push for sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -o xtrace
 docker build -t asia.gcr.io/winter-wonder-647/onion-node:v2 .
-docker run -d -p 8080:8080 asia.gcr.io/winter-wonder-647/onion-node:v2
+#docker run -d -p 8080:8080 asia.gcr.io/winter-wonder-647/onion-node:v2
+gcloud docker push asia.gcr.io/winter-wonder-647/onion-node:v2
